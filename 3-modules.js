e1434dcdfc306665901b7986d5684096f8c23415
Lucian Chiriazi <cmlx20@protonmail.com>
2023-12-14 22:01:33 +0000

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules - Encapsulated Code (only share minimum)
+// CommonJS, every file is module (by default)
+
+const names = require('./3-modules2');
+const sayHi = require('./3-modules3');
+const data = require('./3-modules4');
+require('./3-modules5');
+
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
